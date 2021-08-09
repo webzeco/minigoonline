c65cab9b5701a13dee12e373af99adff7482c835
Abdul Rehman <nodebazaar@gmail.com>
2021-08-09 21:22:43 +0500

    reviews added

--- src/components/common/Reviews.jsx
+++ src/components/common/Reviews.jsx
@@ -20,7 +20,7 @@
 //                  {/* review  */}
 //                  <div class="row align-items-center g-2 ">
 //                 {/* cara */}
-                
+
 //                 {/* cara */}
 //                  {reviews && reviews.map((rev, index) => {
 //                     return (
@@ -37,10 +37,10 @@
 //                             <p class=" re_dis_look overflow-auto">{rev.feedback}</p>
 //                         </div>
 //                       </div>
-                      
+
 //                     );
 //                 })}
-                  
+
 //                  </div>
 //   {/* /review */}
 //                </div>
@@ -95,7 +95,7 @@
 //                     </div>
 //                   </div>
 //                 </div>
- 
+
 //                </div>
 //                <div class="carousel-item" data-bs-interval="3000">
 //                  <div class="row h-100 align-items-center g-2">
@@ -248,66 +248,70 @@ import ProductList from './showProductList';
 import ReactStars from "react-rating-stars-component";
 
 import './style/review.css';
-export default function Reviews({reviews}) {
-  return(
+export default function Reviews({ reviews }) {
+  return (
+    <div>
+       <h5 class="fw-bold fs-3 fs-lg-5 text-center lh-sm mb-3 ">Our Customer's Reviews </h5>
     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-  <div class="carousel-inner">
-  {reviews && reviews.map((rev, index,array) => {
-    console.log(index,array.length);
-    if(array.length<index+3) return null;
-                    return (
-<div class={`carousel-item ${index===0?"active":""} justify-content-center text-center m-5`}>
-                        <div key={index}  class="col-md-4 m-0 mb-3 mb-md-0">
-                        <div class="card card_back_col p-3 Card_h_w ">
-                            <div class="d-flex align-items-center  mt-1 ">
-                            <div class="fw-bold r_title_look m-0 mb-2"> <ReactStars
-                            value={rev.rating}
-                            /></div>
-                              </div>
-                            <div class="fw-bold r_title_look m-0 mb-2">{rev.reviewTitle}</div>
-                            <div class="fw-bold r_name_look mb-3  ">{rev.name}</div>
-                            <p class=" re_dis_look overflow-auto">{rev.feedback}</p>
-                        </div>
-                      </div>
-                      <div key={index}  class="col-md-4 m-0 mb-3 mb-md-0">
-                        <div class="card card_back_col p-3 Card_h_w ">
-                            <div class="d-flex align-items-center mt-1 ">
-                            <ReactStars
-                            value={array[index+1].rating}
-                            />
-                              </div>
-                            <div class="fw-bold r_title_look mb-2">{array[index+1].reviewTitle}</div>
-                            <div class="fw-bold r_name_look mb-3  ">{array[index+1].name}</div>
-                            <p class=" re_dis_look overflow-auto">{array[index+1].feedback}</p>
-                        </div>
-                      </div>
-                      <div key={index}  class="col-md-4  mb-3 mb-md-0">
-                        <div class="card card_back_col p-3 Card_h_w ">
-                            <div class="d-flex align-items-center mt-1 ">
-                            <ReactStars
-                            value={array[index+2].rating}
-                            />
-                              </div>
-                            <div class="fw-bold r_title_look mb-2">{array[index+2].reviewTitle}</div>
-                            <div class="fw-bold r_name_look mb-3  ">{array[index+2].name}</div>
-                            <p class=" re_dis_look overflow-auto">{array[index+2].feedback}</p>
-                        </div>
-                      </div>
+      <div class="carousel-inner">
+        {reviews && reviews.map((rev, index, array) => {
+          console.log(index, array.length);
+          if (array.length>3) {
+            if (array.length < index + 3) return null;
+          }
+          return (
+            <div class={`carousel-item ${index === 0 ? "active" : ""} justify-content-center text-center m-5`}>
+              <div key={index} class="col-md-4 m-0 mb-3 mb-md-0">
+                <div class="card card_back_col p-3 Card_h_w ">
+                  <div class="d-flex align-items-center  mt-1 ">
+                    <div class="fw-bold r_title_look m-0 mb-2"> <ReactStars
+                      value={rev.rating}
+                    /></div>
+                  </div>
+                  <div class="fw-bold r_title_look m-0 mb-2">{rev.reviewTitle}</div>
+                  <div class="fw-bold r_name_look mb-3  ">{rev.name}</div>
+                  <p class=" re_dis_look overflow-auto">{rev.feedback}</p>
+                </div>
+              </div>
+              <div key={index} class="col-md-4 m-0 mb-3 mb-md-0">
+                <div class="card card_back_col p-3 Card_h_w ">
+                  <div class="d-flex align-items-center mt-1 ">
+                    <ReactStars
+                      value={array[index + 1].rating}
+                    />
+                  </div>
+                  <div class="fw-bold r_title_look mb-2">{array[index + 1].reviewTitle}</div>
+                  <div class="fw-bold r_name_look mb-3  ">{array[index + 1].name}</div>
+                  <p class=" re_dis_look overflow-auto">{array[index + 1].feedback}</p>
+                </div>
+              </div>
+              <div key={index} class="col-md-4  mb-3 mb-md-0">
+                <div class="card card_back_col p-3 Card_h_w ">
+                  <div class="d-flex align-items-center mt-1 ">
+                    <ReactStars
+                      value={array[index + 2].rating}
+                    />
+                  </div>
+                  <div class="fw-bold r_title_look mb-2">{array[index + 2].reviewTitle}</div>
+                  <div class="fw-bold r_name_look mb-3  ">{array[index + 2].name}</div>
+                  <p class=" re_dis_look overflow-auto">{array[index + 2].feedback}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
 
-</div>  
-                    );
-                })}
-                  
-                 </div>
-  
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+      </div>
+
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    </div>
   )
 }
